9e3e1f7de23eda6591967473d599fb8a9ecddef9
Jon <jcianci12@gmail.com>
2024-10-25 00:10:08 +0000

    create person working

--- ClientApp/pyscheduler/src/app/people/people.component.ts
+++ ClientApp/pyscheduler/src/app/people/people.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Client, Person } from '../api/api';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup,FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './people.component.html',
   styleUrl: './people.component.css'
 })
 export class PeopleComponent implements OnInit {
-  people:Person[] = [];
+  people: Person[] | undefined;
+  personForm: FormGroup= new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl('')
+  });
+
   constructor(private client: Client) { }
-  ngOnInit() { this.client.peopleAll().subscribe(people => this.people = people); }
+  async ngOnInit() {
+    this.people = await this.client.peopleAll().toPromise();
+    this.personForm 
+  }
+
+  async createPerson(person: Person) {
+    await this.client.peoplePOST(person).toPromise();
+    this.people = await this.client.peopleAll().toPromise();
+  }
+
+  async updatePerson(person: Person) {
+    await this.client.peoplePUT(person.id as number, person).toPromise();
+    this.people = await this.client.peopleAll().toPromise();
+  }
+
+  async deletePerson(personId: number) {
+    await this.client.peopleDELETE(personId).toPromise();
+    this.people = (this.people ?? []).filter(p => p.id !== personId);  }
 }
