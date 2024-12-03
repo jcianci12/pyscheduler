445b5830a9d1d3b161c031dd67525ce2e3890520
jcianci12 <jcianci12@gmail.com>
2024-12-03 20:22:51 +1000

    not working

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,19 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
-import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { AuthModule } from '@auth0/auth0-angular';
+import { provideOAuthClient, AuthConfig, OAuthModuleConfig } from 'angular-oauth2-oidc';
+
+const authConfig: AuthConfig = {
+  issuer: 'https://authentik.tekonline.com.au/application/o/pyscheduler/',
+  redirectUri: window.location.origin + '/index.html',
+  clientId: 'spa-demo',
+  scope: 'openid profile email voucher',
+  strictDiscoveryDocumentValidation: false,
+  skipIssuerCheck: true
+};
+
+const oAuthModuleConfig: OAuthModuleConfig = {
+  ...authConfig,
+  resourceServer: {
+    allowedUrls: ['https://authentik.tekonline.com.au'],
+    sendAccessToken: true
+  }
+};
 
 bootstrapApplication(AppComponent, {
-  ...appConfig,
   providers: [
-    ...appConfig.providers,
-    importProvidersFrom(
-      AuthModule.forRoot({
-        domain: 'your-auth0-domain',
-        clientId: 'your-auth0-client-id',
-      })
-    )
+    provideHttpClient(),
+    provideOAuthClient(oAuthModuleConfig)
   ]
 })
-.catch((err) => console.error(err));
+.catch(err => console.error(err));
