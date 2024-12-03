28d911de31eb19d17d0bdd1e6695ef755ff870ee
jcianci12 <jcianci12@gmail.com>
2024-12-03 17:52:48 +1000

    update to angular

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,12 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { AuthService } from './app/auth/auth.service';
+import { AuthModule } from '@auth0/auth0-angular';
 
-bootstrapApplication(AppComponent, appConfig)
-  .then((moduleRef) => {
-    const authService = moduleRef.injector.get(AuthService);
-    authService.configure();
-  })
-  .catch((err) => console.error(err));
-
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    importProvidersFrom(
+      AuthModule.forRoot({
+        domain: 'your-auth0-domain',
+        clientId: 'your-auth0-client-id',
+      })
+    )
+  ]
+})
+.catch((err) => console.error(err));
