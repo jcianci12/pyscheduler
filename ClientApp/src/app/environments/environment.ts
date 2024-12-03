b90fe8b40737ba5079be92eeab7327c0353ec079
Jon <jcianci12@gmail.com>
2024-12-04 09:01:58 +1000

    env url working

--- ClientApp/src/app/environments/environment.ts
+++ ClientApp/src/app/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:"http://127.0.0.1:5000",
+  baseUrl:"http://localhost:5000",
   tokenUrl:"",
   loginUrl:""
 };
--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -5,13 +5,16 @@ import { AppComponent } from './app/app.component';
 import { provideOAuthClient, AuthConfig, OAuthModuleConfig } from 'angular-oauth2-oidc';
 import { AuthInterceptor } from './app/auth/auth.interceptor';
 import { routes } from './app/app.routes';
+import { environment } from './app/environments/environment';
+import { API_BASE_URL } from './app/api/api';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes), // Provide routes
     provideOAuthClient(),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: API_BASE_URL, useValue: environment.baseUrl } // Provide base URL from environment
   ]
   
 })
