7bec9075e8ddc4754dc145f8c225ca04f31a179f
Jon <jcianci12@gmail.com>
2024-12-11 15:24:03 +1000

    auth code flow working

--- ClientApp/src/app/auth/auth.config.ts
+++ ClientApp/src/app/auth/auth.config.ts
@@ -2,15 +2,18 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://authentik.tekonline.com.au/application/o/pyscheduler/',
-  
-  skipIssuerCheck: true,
-  redirectUri: window.location.origin,
-  clientId: 'yJwnySrODx2x1uNDKzszWiTV3ivrLPBdvvDkz1sN',
-  dummyClientSecret:'test',
-
-  // responseType: 'code',
-  scope: 'email offline_access openid profile ',
-  showDebugInformation: true,
   strictDiscoveryDocumentValidation: false,
+  skipIssuerCheck: true,
+  clientId: 'pysched',
+  responseType: 'code',
+  redirectUri: window.location.origin + '/',
   silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  scope: 'openid profile email api', // Ask offline_access to support refresh token refreshes
+  useSilentRefresh: true, // Needed for Code Flow to suggest using iframe-based refreshes
+  silentRefreshTimeout: 5000, // For faster testing
+  timeoutFactor: 0.25, // For faster testing
+  // sessionChecksEnabled: true,
+  showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
+  clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040,
+  // nonceStateSeparator : 'semicolon' // Real semicolon gets mangled by Duende ID Server's URI encoding
 };
--- ClientApp/src/app/auth/auth.service.ts
+++ ClientApp/src/app/auth/auth.service.ts
@@ -1,21 +1,51 @@
 import { Injectable } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { authConfig } from './auth.config';
+import { BehaviorSubject, filter } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private oauthService: OAuthService) {
-    this.configure();
-  }
+  private isAuthenticatedSubject$ = new BehaviorSubject<boolean>(false);
+  public isAuthenticated$ = this.isAuthenticatedSubject$.asObservable();
 
+  private isDoneLoadingSubject$ = new BehaviorSubject<boolean>(false);
+  public isDoneLoading$ = this.isDoneLoadingSubject$.asObservable();
+  constructor(private oauthService: OAuthService,    private router: Router,
+  ) {
+    this.configure();
+    this.oauthService.events
+      .subscribe(_ => {
+        this.isAuthenticatedSubject$.next(this.oauthService.hasValidAccessToken());
+      });
+    this.isAuthenticatedSubject$.next(this.oauthService.hasValidAccessToken());
+
+    this.oauthService.events
+      .pipe(filter(e => ['token_received'].includes(e.type)))
+      .subscribe(e => this.oauthService.loadUserProfile());
+
+    this.oauthService.events
+      .pipe(filter(e => ['session_terminated', 'session_error'].includes(e.type)))
+      .subscribe(e => this.navigateToLoginPage());
+
+    this.oauthService.setupAutomaticSilentRefresh();
+  }
+  private navigateToLoginPage() {
+    // TODO: Remember current URL
+    this.router.navigateByUrl('/should-login');
+  }
   private configure() {
     this.oauthService.configure(authConfig);
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
     this.oauthService.setupAutomaticSilentRefresh();
   }
 
+
+
+
+  
   login() {
     
         this.oauthService.initLoginFlow();
@@ -27,6 +57,9 @@ export class AuthService {
   isAuthenticated(): boolean {
     return this.oauthService.hasValidAccessToken();
   }
+  public get identityClaims() { return this.oauthService.getIdentityClaims(); }
+  public get idToken() { return this.oauthService.getIdToken(); }
+  public get accessToken() { return this.oauthService.getAccessToken(); }
 
 
 
